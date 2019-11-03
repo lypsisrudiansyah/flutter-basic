ce6226e2972dd011a0f306fa3660b52249f7948b
lypsisrudiansyah <rudiansyahether@gmail.com>
2019-11-03 10:08:37 +0700

    first: First Commit
    basicUsage: Widget MaterialApp, Widget Scaffold, Widget Container, Widget Text etc.

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Fundamental"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 150,
+                child: Text(
+                  "Aplikasi Fundamental Kita, Dan Sedang Melatih Kemampuan Saya",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                      color: Colors.yellowAccent,
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15),
+                ))),
+      ),
+    );
+  }
+}
